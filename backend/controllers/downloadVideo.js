56bb419a3b81665e314385fd6d1141c84d65a3c8
rakibul Islam <irakibul026@gmail.com>
2024-02-20 00:14:58 +0530

    fixed all bugs till now

--- backend/controllers/downloadVideo.js
+++ backend/controllers/downloadVideo.js
@@ -2,82 +2,109 @@ const ytdl = require('ytdl-core');
 const ffmpeg = require('ffmpeg-static');
 const fs = require('fs');
 const { spawn } = require('child_process');
+const cp = require('child_process');
+const readline = require('readline');
+// External modules 
 
 const downloadVideo = async (req, res) => {
     const { videoUrl, formatId } = req.body;
 
     try {
-        if (videoUrl !== "String") {
-            return new Error("Video url must be a String")
-        }
-        // Fetch video info
         const info = await ytdl.getInfo(videoUrl);
+        const videoFilePath = `temp/downloads/_${info.videoDetails.title.slice(0, 2)}_.mkv`;
 
-        // Find the video format with the specified formatId
-        const videoFormat = info.formats.find(format => format.itag === formatId);
+        // Global constants
+        const ref = videoUrl;
+        const tracker = {
+            start: Date.now(),
+            audio: { downloaded: 0, total: Infinity },
+            video: { downloaded: 0, total: Infinity },
+            merged: { frame: 0, speed: '0x', fps: 0 },
+        };
 
-        if (!videoFormat) {
-            return res.status(404).json({ message: 'Video format not found' });
-        }
+        // Get audio and video streams
+        const audio = ytdl(ref, { quality: 'highestaudio' })
+            .on('progress', (_, downloaded, total) => {
+                tracker.audio = { downloaded, total };
+            });
+        const video = ytdl(ref, { quality: formatId })
+            .on('progress', (_, downloaded, total) => {
+                tracker.video = { downloaded, total };
+            });
 
-        // Find the best audio-only format
-        const audioFormat = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
+        // Prepare the progress bar
+        let progressbarHandle = null;
+        const progressbarInterval = 1000;
+        const showProgress = () => {
+            readline.cursorTo(process.stdout, 0);
+            const toMB = i => (i / 1024 / 1024).toFixed(2);
 
-        if (!audioFormat) {
-            return res.status(404).json({ message: 'Audio format not found' });
-        }
+            process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+            process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
 
-        // Create temporary file paths for video and audio
-        const videoFilePath = './temp/downloads/temp_video.mp4';
-        const audioFilePath = './temp/downloads/temp_audio.mp3';
+            process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+            process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
 
-        // Download video using ytdl-core
-        await new Promise((resolve, reject) => {
-            ytdl(videoUrl, { format: videoFormat })
-                .pipe(fs.createWriteStream(videoFilePath))
-                .on('finish', resolve)
-                .on('error', reject);
+            process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+            process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
+            process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+            readline.moveCursor(process.stdout, 0, -3);
+        };
+
+        // Start the ffmpeg child process
+        const ffmpegProcess = cp.spawn(ffmpeg, [
+            // Remove ffmpeg's console spamming
+            '-loglevel', '8', '-hide_banner',
+            // Redirect/Enable progress messages
+            '-progress', 'pipe:3',
+            // Set inputs
+            '-i', 'pipe:4',
+            '-i', 'pipe:5',
+            // Map audio & video from streams
+            '-map', '0:a',
+            '-map', '1:v',
+            // Keep encoding
+            '-c:v', 'copy',
+            // Define output file
+            // 'out.mkv',
+            videoFilePath
+        ], {
+            windowsHide: true,
+            stdio: [
+                /* Standard: stdin, stdout, stderr */
+                'inherit', 'inherit', 'inherit',
+                /* Custom: pipe:3, pipe:4, pipe:5 */
+                'pipe', 'pipe', 'pipe',
+            ],
+        });
+        ffmpegProcess.on('close', () => {
+            console.log('done');
+            // Cleanup
+            process.stdout.write('\n\n\n\n');
+            clearInterval(progressbarHandle);
+            res.status(200).send(fs.readFileSync(videoFilePath))
+            fs.unlinkSync(videoFilePath)
         });
 
-        // Download audio using ytdl-core
-        await new Promise((resolve, reject) => {
-            ytdl(videoUrl)
-                .pipe(fs.createWriteStream(audioFilePath))
-                .on('finish', resolve)
-                .on('error', reject);
-        });
-
-        // Convert video to audio using ffmpeg
-        const ffmpegProcess = spawn(ffmpeg, [
-            '-i', videoFilePath,
-            '-vn', // Disable video
-            '-acodec', 'mp3', // Set audio codec
-            audioFilePath
-        ]);
-
-        ffmpegProcess.on('error', (err) => {
-            console.error('Error converting video to audio:', err);
-            return res.status(500).json({ message: 'Error converting video to audio', error: err });
-        });
-
-        ffmpegProcess.on('close', (code) => {
-            if (code === 0) {
-                // Send audio file as response
-                res.download(audioFilePath, (err) => {
-                    if (err) {
-                        console.error('Error sending file:', err);
-                        return res.status(500).json({ message: 'Error sending file', error: err });
-                    }
-
-                    // Cleanup temporary files
-                    fs.unlinkSync(videoFilePath);
-                    fs.unlinkSync(audioFilePath);
-                });
-            } else {
-                console.error('ffmpeg process exited with non-zero code:', code);
-                return res.status(500).json({ message: 'ffmpeg process exited with non-zero code', code });
+        // Link streams
+        // FFmpeg creates the transformer streams and we just have to insert / read data
+        ffmpegProcess.stdio[3].on('data', chunk => {
+            // Start the progress bar
+            if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+            // Parse the param=value list returned by ffmpeg
+            const lines = chunk.toString().trim().split('\n');
+            const args = {};
+            for (const l of lines) {
+                const [key, value] = l.split('=');
+                args[key.trim()] = value.trim();
             }
+            tracker.merged = args;
         });
+        audio.pipe(ffmpegProcess.stdio[4]);
+        video.pipe(ffmpegProcess.stdio[5]);
+
+
     } catch (error) {
         console.error('Error downloading and converting video:', error);
         return res.status(500).json({ message: 'Error downloading and converting video', error });
